0384122888b59945cc980b646bae8003ccb41017
Panachai <panachai.ny@gmail.com>
2021-08-26 00:14:09 +0700

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,27 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    extraFileExtensions: ['.vue'],
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    jest: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'prettier',
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 }
