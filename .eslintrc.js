87d6e4f52a9acfec466f7c883f73e282c3bf805d
Panachai <panachai.ny@gmail.com>
2021-08-26 00:57:21 +0700

    clean

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    // 'prettier/@typescript-eslint',
-    // 'prettier',
     'plugin:prettier/recommended',
   ],
 }
