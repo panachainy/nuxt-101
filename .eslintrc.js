a0bb4f9784b892deac439af88a6ad28f87742b4c
Panachai <panachai.ny@gmail.com>
2021-08-26 00:55:18 +0700

    work

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,13 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    // 'prettier/@typescript-eslint',
+    // 'prettier',
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
 }
