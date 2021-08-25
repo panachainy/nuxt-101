aa726c280338bba008dffa9caf2fd4cb1467d86a
Panachai <panachai.ny@gmail.com>
2021-08-26 00:19:02 +0700

    Revert "test"
    This reverts commit 0384122888b59945cc980b646bae8003ccb41017.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,16 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-    extraFileExtensions: ['.vue'],
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
   root: true,
   env: {
     browser: true,
-    node: true,
-    jest: true,
+    node: true
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier',
+    'prettier'
   ],
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
 }
